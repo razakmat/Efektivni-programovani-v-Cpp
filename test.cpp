0b63a837afc0787b0c68a3c78d1aef9194859826
Daniel Langr <daniel.langr@fit.cvut.cz>
2021-11-17 11:07:26 +0100

    Add test.cpp

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <utility>
+
+#include "X.h"
+
+int main()
+{
+    X x1(1);
+    X x2(2);
+
+    std::cout << "copy assignment: ";
+    x1 = x2;
+    std::cout << std::endl;
+
+    std::cout << "move assignment: ";
+    x1 = std::move(x2);
+    std::cout << std::endl;
+
+    std::cout << "std::swap: ";
+    std::swap(x1, x2);
+    std::cout << std::endl;
+
+    std::cout << "custom swap: ";
+    swap(x1, x2);
+    std::cout << std::endl;
+}
